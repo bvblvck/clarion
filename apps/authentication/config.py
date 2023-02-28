b5fcb36e8159c1f9009c8d922bce304aa991d314
blackwolf <soluwatoogun@gmail.com>
2023-02-28 11:49:15 -0800

    final

--- apps/authentication/config.py
+++ apps/authentication/config.py
@@ -7,3 +7,5 @@ from django.apps import AppConfig
 class AuthConfig(AppConfig):
     name = 'apps.auth'
     label = 'apps_auth'
+    default_auto_field = 'django.db.models.BigAutoField'
+    verbose_name = "Clarion Web"
--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,9 @@
 from django.contrib import admin
 from django.urls import path, include  # add this
 
-
-
+admin.site.site_title = 'Clarion- Admin panel'
+admin.site.site_header = 'Clarion-Trades'
+admin.site.index_title = 'Administration'
 urlpatterns = [
     path('admin/', admin.site.urls),          # Django admin route
     path("", include("apps.authentication.urls")), # Auth routes - login / register
